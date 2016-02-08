69d8995229deaf554063538e86686bbef05d622b
Cynrick Chiu <cyn.2k5@outlook.com>
2016-02-08 12:46:23 -0800

    Add imports for database functionality

--- kickstarter/__init__.py
+++ kickstarter/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.migrate import Migrate, MigrateCommand
 from flask.ext.script import Manager
 
 app = Flask(__name__)
+app.config.from_object('kickstarter.default_settings')
 manager = Manager(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+from kickstarter.models import *
 
 @app.route("/")
 def index():
